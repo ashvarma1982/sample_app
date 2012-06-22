e894697cfe899df15353e32a78236c5195964320
Ashish Varma <ashish.varma@fireeye.com>
2012-06-22 12:51:58 -0700

    Added helper and example user

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,10 +7,15 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
     
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
-end
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
   end
   
   describe "Help page" do
