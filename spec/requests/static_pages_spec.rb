20adaee874984d8dfedef31a93c2d39c169cd9b4
Ashish Varma <ashish.varma@fireeye.com>
2012-06-25 13:45:14 -0700

    added additional specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,22 @@ describe "StaticPages" do
     before { visit root_path }
     it { should have_selector 'h1', text:'Sample App' }
     it { should have_selector 'title', text: full_title('') }
-    it { should_not have_selector 'title', text: '| Home' }  
+    it { should_not have_selector 'title', text: '| Home' }
+    
+     it "should have the right links on the layout" do
+        visit root_path
+        click_link "About"
+        page.should have_selector 'title', text: full_title('About Us')
+        click_link "Help"
+        page.should have_selector 'title', text: full_title('Help')
+        click_link "Contact"
+        page.should have_selector 'title', text: full_title('Contact')
+        click_link "Home"
+        click_link "Sign up now!"
+        page.should have_selector 'title', text: full_title('Sign up')
+        click_link "sample app"
+        page.should have_selector 'title', text: full_title('')
+    end
   end
   
   describe "Help page" do
