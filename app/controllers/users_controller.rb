871b9ddf839a4bd1167a024cba529e71f091ca35
Ashish Varma <ashish.varma@fireeye.com>
2012-07-11 11:12:59 -0700

    added user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #Handle the successfull save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
 end
